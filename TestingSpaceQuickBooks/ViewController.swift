a35a6581df8b3bd72baafc75e2fd2bca48dd1d18
Manuel Aurora <amp.smichrissoft@gmail.com>
2017-07-19 10:31:12 +0300

    Initial Commit

---
+++ TestingSpaceQuickBooks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestingSpaceQuickBooks
+//
+//  Created by Manuel Aurora on 19.07.17.
+//  Copyright © 2017 Manuel Aurora. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
